c5a5b2e2e803631fdd0538e6f29cc95fada49d06
Nikola <nikoladev06@gmail.com>
2025-03-17 16:26:59 -0300

    grande commit com pasta nova

--- teste_github.java
+++ teste_github.java
@@ -1,6 +1,7 @@
 class Teste{
 
-public static void main(String[] args) {
-    System.out.println("Este é somente um teste de integração com o github");
-}
+    public static void main(String[] args) 
+    {
+        System.out.println("Este é somente um teste de integração com o github");
+    }
 }
