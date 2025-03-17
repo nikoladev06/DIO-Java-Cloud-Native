c7b0564ce81b2dbfc60676d98623b6200f1df477
Nikola <nikoladev06@gmail.com>
2025-03-17 16:30:08 -0300

    sonic

--- Projeto2-teste/src/App.java
+++ Projeto2-teste/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Aprendendo a sincronizar com o github.");
+        System.out.println("Está dando erro.");
     }
 }
