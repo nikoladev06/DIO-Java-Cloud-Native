1b7e990f02edcbb2bd73f2c5ab21ef7880c8428a
Nikola <nikoladev06@gmail.com>
2025-03-17 16:31:25 -0300

    arrumada 2ª linha

--- Projeto2-teste/src/App.java
+++ Projeto2-teste/src/App.java
@@ -1,6 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Aprendendo a sincronizar com o github.");
-        System.out.println("Está dando erro.");
+        System.out.println("Estava dando erro por algum motivo, mas simplesmente funcionou.");
     }
 }
